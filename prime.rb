1c4a310f7cf059291603ad42c57b7fdddc106265
Ming Duan <11203duan@gmail.com>
2019-01-30 22:49:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 def prime?(number)
-  prime = [2, 3, 5, 7, 11, 13, 17, 19, 105557]
-  if prime.include?(number) == true
-    true
-  else false
+  i < number
+  loop do
+  i = 2
+  if number % i == 0
+  else i += 1
+
 end
 end
