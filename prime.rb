5ce1610205babad5ead155b7022ead611709d3d4
Ming Duan <11203duan@gmail.com>
2019-01-31 00:06:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number >= 2
+  i = (2...number).to_a
+  i.all? do |num|
+  number % num != 0
+  end
+elsif number < -2
+  i = (number...-2).to_a
+  i.all? { |num| number % num !=0}
+elsif number = 1 || -1 || 0
+  false
+end
+end
