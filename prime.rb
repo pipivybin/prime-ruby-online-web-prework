7ea671b3c92d017f976b12ed0be326075c701e54
Ming Duan <11203duan@gmail.com>
2019-01-31 00:04:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,12 @@ def prime?(number)
   if number >= 2
   i = (2...number).to_a
   i.all? do |num|
-  number % num == 0
+  number % num != 0
   end
 elsif number < -2
   i = (number...-2).to_a
   i.all? do |num|
-    number % num ==0 
+    number % num !=0
   end
 end
 end
