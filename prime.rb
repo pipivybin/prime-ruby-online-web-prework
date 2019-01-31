ee2c2dbbd026dd8b99907b423f2c2fd586fa9f47
Ming Duan <11203duan@gmail.com>
2019-01-31 00:02:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,11 @@ def prime?(number)
   i = (2...number).to_a
   i.all? do |num|
   number % num != 0
-end
-else number < -2
+  end
+elsif number < -2
   i = (number...-2).to_a
   i.all? do |num|
     number % num !=0 }
-end
+  end
 end
 end
